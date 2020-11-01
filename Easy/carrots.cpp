c6f95ec54644712eaea48afc29c097e43a95697e
Induja <53386868+indujaaaa@users.noreply.github.com>
2020-11-01 19:11:49 +0530

    Create carrots.cpp

---
+++ Easy/carrots.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, p;
+    char str[100];
+    cin >> n >> p;
+    for(int i = 0; i < n; i++)
+        cin>>str;
+    
+    cout << p << endl;
+
+    return 0;
+}
